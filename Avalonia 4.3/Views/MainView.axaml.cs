f86e79e7e1c95ee1723af59ad3fdf3aa9dc57396
Petr-Vobla <154663575+Petr-Vobla@users.noreply.github.com>
2024-05-09 19:51:35 +0300

    Add files via upload

---
+++ Avalonia 4.3/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace Avalonia_4._3.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Avalonia 4.3/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace Avalonia_4._3.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
